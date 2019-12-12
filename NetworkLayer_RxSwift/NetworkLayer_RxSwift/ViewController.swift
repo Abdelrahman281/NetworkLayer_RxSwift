6f876bc7a416bf24c261cc00e8e39d23ed66a1d7
Abdelrahman <abdelrahman.mahmoud.r92@gmail.com>
2019-12-12 15:07:27 +0200

    ADD: App Creation

---
+++ NetworkLayer_RxSwift/NetworkLayer_RxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayer_RxSwift
+//
+//  Created by Abdelrahman Mahmoud on 12/12/19.
+//  Copyright © 2019 Abdelrahman Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NetworkLayer_RxSwift/NetworkLayer_RxSwiftTests/NetworkLayer_RxSwiftTests.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkLayer_RxSwiftTests.swift
+//  NetworkLayer_RxSwiftTests
+//
+//  Created by Abdelrahman Mahmoud on 12/12/19.
+//  Copyright © 2019 Abdelrahman Mahmoud. All rights reserved.
+//
+
+import XCTest
+@testable import NetworkLayer_RxSwift
+
+class NetworkLayer_RxSwiftTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}

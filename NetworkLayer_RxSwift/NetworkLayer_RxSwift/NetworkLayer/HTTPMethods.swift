2ba5a2138db77d1a130a4d40e5bec8c6450bcbda
Abdelrahman <abdelrahman.mahmoud.r92@gmail.com>
2019-12-12 22:17:39 +0200

    ADD: HTTP Methods

---
+++ NetworkLayer_RxSwift/NetworkLayer_RxSwift/NetworkLayer/HTTPMethods.swift
@@ -0,0 +1,24 @@
+//
+//  HTTPMethods.swift
+//  NetworkLayer_RxSwift
+//
+//  Created by Abdelrahman Mahmoud on 12/12/19.
+//  Copyright © 2019 Abdelrahman Mahmoud. All rights reserved.
+//
+
+import Foundation
+
+
+//Enum for HTTP Methods cases.
+
+enum HTTPMethod: String {
+    case CONNECT = "CONNECT"
+    case DELETE = "DELETE"
+    case GET = "GET"
+    case HEAD = "HEAD"
+    case OPTIONS = "OPTIONS"
+    case PATCH = "PATCH"
+    case POST = "POST"
+    case PUT = "PUT"
+    case TRACE = "TRACE"
+}
